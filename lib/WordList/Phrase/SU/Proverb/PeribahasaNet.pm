852f0861fcba989d67ec10b72e93ef988948d40b
perlancar (@pc-office) <perlancar@gmail.com>
2016-02-10 18:11:33 +0700

    Initial commit

---
+++ lib/WordList/Phrase/SU/Proverb/PeribahasaNet.pm
@@ -0,0 +1,22 @@
+package WordList::Phrase::SU::Proverb::PeribahasaNet;
+
+# DATE
+# VERSION
+
+use utf8;
+
+use WordList;
+our @ISA = qw(WordList);
+
+# STATS
+
+1;
+# ABSTRACT: Sundanese proverbs from peribahasa.net
+
+=head1 SEE ALSO
+
+L<http://www.peribahasa.net/peribahasa-sunda.php>
+
+=cut
+
+__DATA__
